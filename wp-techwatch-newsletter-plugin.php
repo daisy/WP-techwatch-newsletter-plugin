c7ee15069aba273996125b6f4ad467ea6b4737c0
Bradford Knowlton <brad@ucanbonline.net>
2014-06-22 22:32:44 -0600

    Initial plugin commit

---
+++ wp-techwatch-newsletter-plugin.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: WP Techwatch Newsletter Plugin
+Plugin URI: http://www.daisy.org/
+Description: Used by millions to make WP better.
+Author: Bradford Knowton
+Version: 1.16
+Author URI: http://bradknowlton.com/
+License: GPLv2 or later
+
+*/
