5a7ee08a2f2c0a43a768b6e27e9b850727551efc
Bradford Knowlton <bradford.knowlton@gmail.com>
2014-07-14 14:50:08 -0600

    Changed Plugin Name and Case of TW

--- wp-techwatch-newsletter-plugin.php
+++ wp-techwatch-newsletter-plugin.php
@@ -1,37 +1,37 @@
 <?php
 
 /*
-Plugin Name: WP Techwatch Newsletter Plugin
+Plugin Name: DAISY TechWatch Newsletter Plugin
 Plugin URI: http://www.daisy.org/
-Description: Used by millions to make WP better.
+Description: Adds custom post type for DAISY TechWatch Newsletter Plugin
 Author: Bradford Knowton
-Version: 1.18
+Version: 1.18.1
 Author URI: http://bradknowlton.com/
 License: GPLv2 or later
-GitHub Plugin URI: https://github.com/daisy/WP-techwatch-newsletter-plugin
+GitHub Plugin URI: https://github.com/daisy/WP-TechWatch-newsletter-plugin
 GitHub Branch:     master
 */
 
-class WPTechwatchNewsletterPlugin {
+class WPTechWatchNewsletterPlugin {
 
 	/*--------------------------------------------*
 	 * Constants
 	 *--------------------------------------------*/
-	const name = 'WP Techwatch Newsletter Plugin';
-	const slug = 'wp_techwatch_newsletter_plugin';
+	const name = 'WP TechWatch Newsletter Plugin';
+	const slug = 'wp_TechWatch_newsletter_plugin';
 
 	/**
 	 * Constructor
 	 */
 	function __construct() {
 		//Hook up to the init action
-		add_action( 'init', array( &$this, 'init_wp_techwatch_newsletter_plugin' ) );
+		add_action( 'init', array( &$this, 'init_wp_TechWatch_newsletter_plugin' ) );
 	}
 
 	/**
 	 * Runs when the plugin is initialized
 	 */
-	function init_wp_techwatch_newsletter_plugin() {
+	function init_wp_TechWatch_newsletter_plugin() {
 		// Load JavaScript and stylesheets
 		$this->register_scripts_and_styles();
 
@@ -43,18 +43,18 @@ class WPTechwatchNewsletterPlugin {
 		}
 
 		$labels = array(
-			'name' => _x( 'TechWatch', 'techwatch' ),
-			'singular_name' => _x( 'TechWatch', 'techwatch' ),
-			'add_new' => _x( 'Add New', 'techwatch' ),
-			'add_new_item' => _x( 'Add New TechWatch', 'techwatch' ),
-			'edit_item' => _x( 'Edit TechWatch', 'techwatch' ),
-			'new_item' => _x( 'New TechWatch', 'techwatch' ),
-			'view_item' => _x( 'View TechWatch', 'techwatch' ),
-			'search_items' => _x( 'Search TechWatch', 'techwatch' ),
-			'not_found' => _x( 'No techwatch found', 'techwatch' ),
-			'not_found_in_trash' => _x( 'No techwatch found in Trash', 'techwatch' ),
-			'parent_item_colon' => _x( 'Parent TechWatch:', 'techwatch' ),
-			'menu_name' => _x( 'TechWatch', 'techwatch' ),
+			'name' => _x( 'TechWatch', 'TechWatch' ),
+			'singular_name' => _x( 'TechWatch', 'TechWatch' ),
+			'add_new' => _x( 'Add New', 'TechWatch' ),
+			'add_new_item' => _x( 'Add New TechWatch', 'TechWatch' ),
+			'edit_item' => _x( 'Edit TechWatch', 'TechWatch' ),
+			'new_item' => _x( 'New TechWatch', 'TechWatch' ),
+			'view_item' => _x( 'View TechWatch', 'TechWatch' ),
+			'search_items' => _x( 'Search TechWatch', 'TechWatch' ),
+			'not_found' => _x( 'No TechWatch found', 'TechWatch' ),
+			'not_found_in_trash' => _x( 'No TechWatch found in Trash', 'TechWatch' ),
+			'parent_item_colon' => _x( 'Parent TechWatch:', 'TechWatch' ),
+			'menu_name' => _x( 'TechWatch', 'TechWatch' ),
 		);
 		$args = array(
 			'labels' => $labels,
@@ -73,7 +73,7 @@ class WPTechwatchNewsletterPlugin {
 			'capability_type' => 'post',
 			'menu_icon' => 'dashicons-search'
 		);
-		register_post_type( 'techwatch', $args );
+		register_post_type( 'TechWatch', $args );
 
 	}
 
@@ -91,4 +91,4 @@ class WPTechwatchNewsletterPlugin {
 
 } // end class
 
-new WPTechwatchNewsletterPlugin();
+new WPTechWatchNewsletterPlugin();
